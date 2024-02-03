93ded324394bc8c72dc20a0a3c7d5eadb8bfbf44
Ghaleb Hamadi <ghaleb.hamadi@outlook.com>
2024-02-03 11:29:29 +0200

    add a carousel component

---
+++ src/carousel.js
@@ -0,0 +1,45 @@
+export function addCarouselEventListeners(carouselSelector) {
+  const carousel = document.querySelector(carouselSelector);
+
+  if (!carousel) {
+    throw new Error(`No carousel found for selector: ${carouselSelector}`);
+  }
+
+  const prevButton = carousel.querySelector('.js-prev');
+  const nextButton = carousel.querySelector('.js-next');
+  const slidesWrapper = carousel.querySelector('.js-slides-wrapper');
+  const indicators = carousel.querySelectorAll('.js-indicator');
+
+  const slideWidth = slidesWrapper.clientWidth;
+  const slidesCount = slidesWrapper.children.length;
+  let index = 0;
+
+  const updateIndicators = () => {
+    indicators.forEach((indicator, i) => {
+      if (i === index) {
+        indicator.classList.add('bg-primary');
+        indicator.classList.remove('bg-[#C4C4C4]');
+      } else {
+        indicator.classList.add('bg-[#C4C4C4]');
+        indicator.classList.remove('bg-primary');
+      }
+    });
+  };
+
+  const showCurrentCarouselSlide = () => {
+    slidesWrapper.style.transform = `translateX(-${index * slideWidth}px)`;
+    updateIndicators();
+  };
+
+  nextButton.addEventListener('click', () => {
+    index = (index + 1) % slidesCount;
+    showCurrentCarouselSlide();
+  });
+
+  prevButton.addEventListener('click', () => {
+    index = index - 1 < 0 ? slidesCount - 1 : index - 1;
+    showCurrentCarouselSlide();
+  });
+
+  slidesWrapper.appendChild;
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
 import './index.css';
+import { addCarouselEventListeners } from './carousel';
+
+window.onload = () => {
+  addCarouselEventListeners('.js-carousel');
+};
