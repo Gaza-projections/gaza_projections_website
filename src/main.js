0a4724c95ad2e14a9617b66031b103f55ed45a37
Teddy Zeenny <teddyzeenny@gmail.com>
2024-02-12 00:44:59 +0200

    More menu fixes and mobile menu
    Co-authored with @ghamadi

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import '@splidejs/splide/css/core';
 import './index.css';
 import Splide from '@splidejs/splide';
 import { Accordion } from './accordion';
+import { initSectionObserver } from './section-observer';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Splide('.js-carousel').mount();
@@ -12,4 +13,14 @@ document.addEventListener('DOMContentLoaded', () => {
   accordions.forEach((accordionEl) => {
     new Accordion(accordionEl);
   });
+
+  const navAccordion = new Accordion(document.querySelector('.js-nav'));
+
+  // Close nav accordion (on mobile) when a link is clicked
+  document.getElementById('nav-list').addEventListener('click', () => {
+    navAccordion.close();
+  });
+
+  // Updates URL based on section
+  initSectionObserver();
 });
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
-  base: "/gaza_projections_website/",
+  base: '/gaza_projections_website/',
+  server: {
+    host: '0.0.0.0'
+  }
 });
