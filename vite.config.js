f91eea70b163bd6cfc6f24658036fc3e1138ce5a
Teddy Zeenny <teddyzeenny@gmail.com>
2024-02-13 21:53:54 +0200

    Disable GA in development

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,27 @@
 import { defineConfig } from 'vite';
 import { ViteEjsPlugin } from 'vite-plugin-ejs';
 
+const env = process.env.NODE_ENV;
+
+const ejsConfig = {
+  development: {
+    googleAnalyticsId: null,
+    mailerLite: {
+      accountId: '817099',
+      formId: '112449172135741241',
+      code: '12133001'
+    }
+  },
+  production: {
+    googleAnalyticsId: 'G-N3FPXE95K7',
+    mailerLite: {
+      accountId: '817099',
+      formId: '112449172135741241',
+      code: '12133001'
+    }
+  }
+}[env];
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/gaza_projections_website/',
@@ -9,12 +30,8 @@ export default defineConfig({
   },
   plugins: [
     ViteEjsPlugin({
-      googleAnalyticsId: 'G-N3FPXE95K7',
-      mailerLite: {
-        accountId: '817099',
-        formId: '112449172135741241',
-        code: '12133001'
-      }
+      env,
+      ...ejsConfig
     })
   ]
 });
